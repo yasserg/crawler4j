497d71e24e24d5eab9a49b80dcfbd2ec2289e5d7
Yasser Ganjisaffar <ganjisaffar@gmail.com>
2012-01-01 15:12:42 -0800

    Cleaned basic example.

--- src/test/java/edu/uci/ics/crawler4j/examples/basic/BasicCrawler.java
+++ src/test/java/edu/uci/ics/crawler4j/examples/basic/BasicCrawler.java
@@ -33,14 +33,21 @@ public class BasicCrawler extends WebCrawler {
 	private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|bmp|gif|jpe?g" + "|png|tiff?|mid|mp2|mp3|mp4"
 			+ "|wav|avi|mov|mpeg|ram|m4v|pdf" + "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
 
-	private final static String DOMAIN = "http://www.ics.uci.edu/";
-
+	/**
+	 * You should implement this function to specify whether
+	 * the given url should be crawled or not (based on your
+	 * crawling logic).
+	 */
 	@Override
 	public boolean shouldVisit(WebURL url) {
 		String href = url.getURL().toLowerCase();
-		return !FILTERS.matcher(href).matches() && href.startsWith(DOMAIN);
+		return !FILTERS.matcher(href).matches() && href.startsWith("http://www.ics.uci.edu/");
 	}
 
+	/**
+	 * This function is called when a page is fetched and ready 
+	 * to be processed by your program.
+	 */
 	@Override
 	public void visit(Page page) {
 		int docid = page.getWebURL().getDocid();
