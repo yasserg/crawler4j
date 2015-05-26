28ca8ebb6068826104665f40b60957287df73653
Egbert van der Wal <ewal@pointpro.nl>
2015-05-26 12:25:54 +0200

    added a callback for unhandled exceptions during fetching

--- src/main/java/edu/uci/ics/crawler4j/crawler/WebCrawler.java
+++ src/main/java/edu/uci/ics/crawler4j/crawler/WebCrawler.java
@@ -212,7 +212,19 @@ public class WebCrawler implements Runnable {
     // Do nothing by default (except basic logging)
     // Sub-classed can override this to add their custom functionality
   }
-
+  
+  /**
+   * This function is called when a unhandled exception was encountered during fetching
+   *
+   * @param webUrl URL where a unhandled exception occured
+   */
+  protected void onUnhandledException(WebURL webUrl, Throwable e) {
+    logger.warn("Unhandled exception while fetching {}: {}", webUrl.getURL(), e.getMessage());
+    logger.info("Stacktrace: ", e);
+    // Do nothing by default (except basic logging)
+    // Sub-classed can override this to add their custom functionality
+  }
+  
   /**
    * This function is called if there has been an error in parsing the content.
    *
@@ -421,6 +433,7 @@ public class WebCrawler implements Runnable {
       String urlStr = (curURL == null ? "NULL" : curURL.getURL());
       logger.error("{}, while processing: {}", e.getMessage(), urlStr);
       logger.debug("Stacktrace", e);
+      onUnhandledException(curURL, e);
     } finally {
       if (fetchResult != null) {
         fetchResult.discardContentIfNotConsumed();
