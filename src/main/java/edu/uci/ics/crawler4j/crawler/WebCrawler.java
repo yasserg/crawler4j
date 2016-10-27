d08bf8738680178b111ce07d9b4593464125fe3a
bgoldowsky <bgoldowsky@cast.org>
2016-10-27 13:40:49 -0400

    Add shouldFollowLinksInPage(Page) method, to allow easy configuration for certain crawling policies. Fixes https://github.com/yasserg/crawler4j/issues/129

--- src/main/java/edu/uci/ics/crawler4j/crawler/WebCrawler.java
+++ src/main/java/edu/uci/ics/crawler4j/crawler/WebCrawler.java
@@ -302,15 +302,20 @@ public class WebCrawler implements Runnable {
   }
 
   /**
-   * Determine whether links in the given page should be added to the queue for crawling.
+   * Determine whether links found at the given URL should be added to the queue for crawling.
    * By default this method returns true always, but classes that extend WebCrawler can
    * override it in order to implement particular policies about which pages should be
    * mined for outgoing links and which should not.
    *
-   * @param page the page currently being visited
+   * If links from the URL are not being followed, then we are not operating as
+   * a web crawler and need not check robots.txt before fetching the single URL.
+   * (see definition at http://www.robotstxt.org/faq/what.html).  Thus URLs that
+   * return false from this method will not be subject to robots.txt filtering.
+   *
+   * @param url the URL of the page under consideration
    * @return true if outgoing links from this page should be added to the queue.
    */
-  protected boolean shouldFollowLinksInPage(Page page) {
+  protected boolean shouldFollowLinksIn(WebURL url) {
     return true;
   }
 
@@ -368,7 +373,7 @@ public class WebCrawler implements Runnable {
             webURL.setDocid(-1);
             webURL.setAnchor(curURL.getAnchor());
             if (shouldVisit(page, webURL)) {
-              if (robotstxtServer.allows(webURL)) {
+              if (!shouldFollowLinksIn(webURL) || robotstxtServer.allows(webURL)) {
                 webURL.setDocid(docIdServer.getNewDocID(movedToUrl));
                 frontier.schedule(webURL);
               } else {
@@ -401,7 +406,7 @@ public class WebCrawler implements Runnable {
 
         parser.parse(page, curURL.getURL());
 
-        if (shouldFollowLinksInPage(page)) {
+        if (shouldFollowLinksIn(page.getWebURL())) {
           ParseData parseData = page.getParseData();
           List<WebURL> toSchedule = new ArrayList<>();
           int maxCrawlDepth = myController.getConfig().getMaxDepthOfCrawling();
@@ -418,7 +423,7 @@ public class WebCrawler implements Runnable {
               webURL.setDepth((short) (curURL.getDepth() + 1));
               if ((maxCrawlDepth == -1) || (curURL.getDepth() < maxCrawlDepth)) {
                 if (shouldVisit(page, webURL)) {
-                  if (robotstxtServer.allows(webURL)) {
+                  if (!shouldFollowLinksIn(webURL) || robotstxtServer.allows(webURL)) {
                     webURL.setDocid(docIdServer.getNewDocID(webURL.getURL()));
                     toSchedule.add(webURL);
                   } else {
