639c9f4d25aff3b5073d74cf20de0f2b5b741d76
Federico Tolomei <mail+github@s17t.net>
2017-08-02 18:09:06 +0200

    Merge pull request #223

--- src/main/java/edu/uci/ics/crawler4j/crawler/Page.java
+++ src/main/java/edu/uci/ics/crawler4j/crawler/Page.java
@@ -108,43 +108,6 @@ public class Page {
     }
 
     /**
-     * Loads the content of this page from a fetched HttpEntity.
-     *
-     * @param entity HttpEntity
-     * @param maxBytes The maximum number of bytes to read
-     * @throws Exception when load fails
-     */
-    public void load(HttpEntity entity, int maxBytes) throws Exception {
-
-        contentType = null;
-        Header type = entity.getContentType();
-        if (type != null) {
-            contentType = type.getValue();
-        }
-
-        contentEncoding = null;
-        Header encoding = entity.getContentEncoding();
-        if (encoding != null) {
-            contentEncoding = encoding.getValue();
-        }
-
-        Charset charset = null;
-
-        try {
-            charset = ContentType.getOrDefault(entity).getCharset();
-        } catch (Exception e) {
-            logger.warn("parse charset failed", e);
-            charset = Charset.forName("UTF-8");
-        }
-
-        if (charset != null) {
-            contentCharset = charset.displayName();
-        }
-
-        contentData = toByteArray(entity, maxBytes);
-    }
-
-    /**
      * Read contents from an entity, with a specified maximum. This is a replacement of
      * EntityUtils.toByteArray because that function does not impose a maximum size.
      *
@@ -188,6 +151,42 @@ public class Page {
             }
             return buffer.toByteArray();
         }
+    }
+
+    /**
+     * Loads the content of this page from a fetched HttpEntity.
+     *
+     * @param entity HttpEntity
+     * @param maxBytes The maximum number of bytes to read
+     * @throws Exception when load fails
+     */
+    public void load(HttpEntity entity, int maxBytes) throws Exception {
+
+        contentType = null;
+        Header type = entity.getContentType();
+        if (type != null) {
+            contentType = type.getValue();
+        }
+
+        contentEncoding = null;
+        Header encoding = entity.getContentEncoding();
+        if (encoding != null) {
+            contentEncoding = encoding.getValue();
+        }
+
+        Charset charset;
+        try {
+            charset = ContentType.getOrDefault(entity).getCharset();
+        } catch (Exception e) {
+            logger.warn("parse charset failed: {}", e.getMessage());
+            charset = Charset.forName("UTF-8");
+        }
+
+        if (charset != null) {
+            contentCharset = charset.displayName();
+        }
+
+        contentData = toByteArray(entity, maxBytes);
     }
 
     public WebURL getWebURL() {
