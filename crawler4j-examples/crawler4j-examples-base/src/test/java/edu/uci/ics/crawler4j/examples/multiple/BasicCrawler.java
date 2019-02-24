13afe9460c1e72ffebe1b14c260ae0fd2eaa5f5e
Yasser Ganjisaffar <yasserg@users.noreply.github.com>
2019-02-24 10:06:36 -0800

    Use factory in multiple-crawlers example

--- crawler4j-examples/crawler4j-examples-base/src/test/java/edu/uci/ics/crawler4j/examples/multiple/BasicCrawler.java
+++ crawler4j-examples/crawler4j-examples-base/src/test/java/edu/uci/ics/crawler4j/examples/multiple/BasicCrawler.java
@@ -1,36 +1,19 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package edu.uci.ics.crawler4j.examples.multiple;
 
+import java.util.List;
 import java.util.Set;
 import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.ImmutableList;
+
 import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
 import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
 
-/**
- * @author Yasser Ganjisaffar
- */
 public class BasicCrawler extends WebCrawler {
     private static final Logger logger = LoggerFactory.getLogger(BasicCrawler.class);
 
@@ -38,11 +21,10 @@ public class BasicCrawler extends WebCrawler {
         ".*(\\.(css|js|bmp|gif|jpe?g" + "|png|tiff?|mid|mp2|mp3|mp4" +
         "|wav|avi|mov|mpeg|ram|m4v|pdf" + "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
 
-    private String[] myCrawlDomains;
+    private final List<String> myCrawlDomains;
 
-    @Override
-    public void onStart() {
-        myCrawlDomains = (String[]) myController.getCustomData();
+    public BasicCrawler(List<String> myCrawlDomains) {
+        this.myCrawlDomains = ImmutableList.copyOf(myCrawlDomains);
     }
 
     @Override
--- crawler4j-examples/crawler4j-examples-base/src/test/java/edu/uci/ics/crawler4j/examples/multiple/MultipleCrawlerController.java
+++ crawler4j-examples/crawler4j-examples-base/src/test/java/edu/uci/ics/crawler4j/examples/multiple/MultipleCrawlerController.java
@@ -1,24 +1,11 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package edu.uci.ics.crawler4j.examples.multiple;
+
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.ImmutableList;
 
 import edu.uci.ics.crawler4j.crawler.CrawlConfig;
 import edu.uci.ics.crawler4j.crawler.CrawlController;
@@ -26,33 +13,18 @@ import edu.uci.ics.crawler4j.fetcher.PageFetcher;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
-/**
- * @author Yasser Ganjisaffar
- */
-
 public class MultipleCrawlerController {
     private static final Logger logger = LoggerFactory.getLogger(MultipleCrawlerController.class);
 
     public static void main(String[] args) throws Exception {
-        if (args.length != 1) {
-            logger.info("Needed parameter: ");
-            logger.info("\t rootFolder (it will contain intermediate crawl data)");
-            return;
-        }
-
-    /*
-     * crawlStorageFolder is a folder where intermediate crawl data is
-     * stored.
-     */
-        String crawlStorageFolder = args[0];
+        // The folder where intermediate crawl data is stored (e.g. list of urls that are extracted from previously
+        // fetched pages and need to be crawled later).
+        String crawlStorageFolder = "/tmp/crawler4j/";
 
         CrawlConfig config1 = new CrawlConfig();
         CrawlConfig config2 = new CrawlConfig();
 
-    /*
-     * The two crawlers should have different storage folders for their
-     * intermediate data
-     */
+        // The two crawlers should have different storage folders for their intermediate data.
         config1.setCrawlStorageFolder(crawlStorageFolder + "/crawler1");
         config2.setCrawlStorageFolder(crawlStorageFolder + "/crawler2");
 
@@ -62,26 +34,19 @@ public class MultipleCrawlerController {
         config1.setMaxPagesToFetch(50);
         config2.setMaxPagesToFetch(100);
 
-    /*
-     * We will use different PageFetchers for the two crawlers.
-     */
+        // We will use different PageFetchers for the two crawlers.
         PageFetcher pageFetcher1 = new PageFetcher(config1);
         PageFetcher pageFetcher2 = new PageFetcher(config2);
 
-    /*
-     * We will use the same RobotstxtServer for both of the crawlers.
-     */
+        // We will use the same RobotstxtServer for both of the crawlers.
         RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
         RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher1);
 
         CrawlController controller1 = new CrawlController(config1, pageFetcher1, robotstxtServer);
         CrawlController controller2 = new CrawlController(config2, pageFetcher2, robotstxtServer);
 
-        String[] crawler1Domains = {"https://www.ics.uci.edu/", "https://www.cnn.com/"};
-        String[] crawler2Domains = {"https://en.wikipedia.org/"};
-
-        controller1.setCustomData(crawler1Domains);
-        controller2.setCustomData(crawler2Domains);
+        List<String> crawler1Domains = ImmutableList.of("https://www.ics.uci.edu/", "https://www.cnn.com/");
+        List<String> crawler2Domains = ImmutableList.of("https://en.wikipedia.org/");
 
         controller1.addSeed("https://www.ics.uci.edu/");
         controller1.addSeed("https://www.cnn.com/");
@@ -92,12 +57,12 @@ public class MultipleCrawlerController {
         controller2.addSeed("https://en.wikipedia.org/wiki/Obama");
         controller2.addSeed("https://en.wikipedia.org/wiki/Bing");
 
-    /*
-     * The first crawler will have 5 concurrent threads and the second
-     * crawler will have 7 threads.
-     */
-        controller1.startNonBlocking(BasicCrawler.class, 5);
-        controller2.startNonBlocking(BasicCrawler.class, 7);
+        CrawlController.WebCrawlerFactory<BasicCrawler> factory1 = () -> new BasicCrawler(crawler1Domains);
+        CrawlController.WebCrawlerFactory<BasicCrawler> factory2 = () -> new BasicCrawler(crawler2Domains);
+
+        // The first crawler will have 5 concurrent threads and the second crawler will have 7 threads.
+        controller1.startNonBlocking(factory1, 5);
+        controller2.startNonBlocking(factory2, 7);
 
         controller1.waitUntilFinish();
         logger.info("Crawler 1 is finished.");
