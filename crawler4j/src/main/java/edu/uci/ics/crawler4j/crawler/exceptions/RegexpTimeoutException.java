c659aa6a68dab4e4303dd74cadb2cd6d775a636c
dgoiko <12698425+dgoiko@users.noreply.github.com>
2020-01-24 21:33:48 +0100

    PathRules that timeout if regex takes too long
    TimeoutablePathRule adds support to timeout Regexp that run for too long. You can configure them to consider that a timeout means a match, or a not match.
    
    Personally I'd throw RegexpTimeoutException, but it may break some foreign subclasses, so I decided to stick to return false. The static version, matchesRobotsPattern throws RegexpTimeoutException if configured to fail on timeout since it will not break any existing code.

---
+++ crawler4j/src/main/java/edu/uci/ics/crawler4j/crawler/exceptions/RegexpTimeoutException.java
@@ -0,0 +1,59 @@
+package edu.uci.ics.crawler4j.crawler.exceptions;
+
+public class RegexpTimeoutException extends RuntimeException{
+    private static final long serialVersionUID = 6437153127902393756L;
+
+    private final String regularExpression;
+
+    private final String stringToMatch;
+
+    private final long timeoutMillis;
+
+	public RegexpTimeoutException() {
+		super();
+		regularExpression = null;
+		stringToMatch = null;
+		timeoutMillis = 0;
+	}
+
+	public RegexpTimeoutException(String message, Throwable cause) {
+		super(message, cause);
+		regularExpression = null;
+		stringToMatch = null;
+		timeoutMillis = 0;
+	}
+
+	public RegexpTimeoutException(String message) {
+		super(message);
+		regularExpression = null;
+		stringToMatch = null;
+		timeoutMillis = 0;
+	}
+
+	public RegexpTimeoutException(Throwable cause) {
+		super(cause);
+		regularExpression = null;
+		stringToMatch = null;
+		timeoutMillis = 0;
+	}
+
+	public RegexpTimeoutException(String regularExpression, String stringToMatch, long timeoutMillis) {
+		super("Timeout occurred after " + timeoutMillis + "ms while processing regular expression '"  + regularExpression + "' on input '" + stringToMatch + "'!");
+		this.regularExpression = regularExpression;
+		this.stringToMatch = stringToMatch;
+		this.timeoutMillis = timeoutMillis;
+	}
+
+	public String getRegularExpression() {
+		return regularExpression;
+	}
+
+	public String getStringToMatch() {
+		return stringToMatch;
+	}
+
+	public long getTimeoutMillis() {
+		return timeoutMillis;
+	}
+
+}
