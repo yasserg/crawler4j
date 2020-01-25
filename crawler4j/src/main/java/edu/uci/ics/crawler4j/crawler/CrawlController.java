048c065279893fbf7f02607819e873860f61ed02
dgoiko <12698425+dgoiko@users.noreply.github.com>
2020-01-25 17:07:05 +0100

    CrawlController createFrontier.
    Now CrawlController subclasses can create custom Frontiers.

--- crawler4j/src/main/java/edu/uci/ics/crawler4j/crawler/CrawlController.java
+++ crawler4j/src/main/java/edu/uci/ics/crawler4j/crawler/CrawlController.java
@@ -141,7 +141,7 @@ public class CrawlController {
 
         env = new Environment(envHome, envConfig);
         docIdServer = new DocIDServer(env, config);
-        frontier = new Frontier(env, config);
+        frontier = createFrontier(config);
 
         this.pageFetcher = pageFetcher;
         this.parser = parser == null ? new Parser(config, tldList) : parser;
@@ -151,6 +151,15 @@ public class CrawlController {
         shuttingDown = false;
 
         robotstxtServer.setCrawlConfig(config);
+    }
+
+    /**
+     * Creates the Frontier for this instance. Subclasses can create custom Frontiers
+     * @param config configuration procided to the CrawlController
+     * @return
+     */
+    protected Frontier createFrontier(CrawlConfig config) {
+        return new Frontier(env, config);
     }
 
     public Parser getParser() {
